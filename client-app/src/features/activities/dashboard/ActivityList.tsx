436ce011058168873344909096ac860be26948a7
Yevhen Yachmenov <y.yachmenov@expoplatform.com>
2023-04-29 14:58:40 +0300

    Add deleting activities

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -4,10 +4,11 @@ import { Activity } from "../../../app/models/activity";
 
 interface Props {
     activities: Activity[];
-    selectActivity: (id: string) => void
+    selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void;
 }
 
-export default function ActivityList({activities, selectActivity}: Props) {
+export default function ActivityList({activities, selectActivity, deleteActivity}: Props) {
     return (
         <Segment>
             <Item.Group divided>
@@ -21,6 +22,10 @@ export default function ActivityList({activities, selectActivity}: Props) {
                                 <div>{activity.city}, {activity.venue}</div>
                             </Item.Description>
                             <Item.Extra>
+                                <Button
+                                    onClick={() => deleteActivity(activity.id)} 
+                                    floated='right' content='Delete' color='red'
+                                />
                                 <Button
                                     onClick={() => selectActivity(activity.id)} 
                                     floated='right' content='View' color='blue'
