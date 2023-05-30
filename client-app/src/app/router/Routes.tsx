aa1a99b8f294c4183c1c7afe66ce01ccaea8792d
Yevhen <yeo210213@gmail.com>
2023-05-30 16:51:54 +0300

    Add errors handling

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,8 +1,11 @@
-import { RouteObject, createBrowserRouter } from "react-router-dom";
+import { Navigate, RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../layout/App";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import AcitvityForm from "../../features/activities/form/AcitvityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import TestErrors from "../../features/errors/TestError";
+import NotFound from "../../features/errors/NotFound";
+import ServerError from "../../features/errors/ServerError";
 
 export const routes: RouteObject[] = [
     {
@@ -13,6 +16,10 @@ export const routes: RouteObject[] = [
             {path: 'activities/:id', element: <ActivityDetails />},
             {path: 'createActivity', element: <AcitvityForm key='create' />},
             {path: 'editActivity/:id', element: <AcitvityForm key='edit' />},
+            {path: 'errors', element: <TestErrors />},
+            {path: 'server-error', element: <ServerError />},
+            {path: 'not-found', element: <NotFound />},
+            {path: '*', element: <Navigate replace to='/not-found' />},
         ]
     }
 ];
